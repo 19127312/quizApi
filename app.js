7cde6a7a1fdc3ff684098392160662a8f14e5048
19127312 <63494302+19127312@users.noreply.github.com>
2022-10-28 23:58:10 +0700

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: '*',
   credentials: true,            //access-control-allow-credentials:true
   optionSuccessStatus: 200
 }
